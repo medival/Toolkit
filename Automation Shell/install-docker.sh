027490dc59be67e7e191ff1c516fa887a77b9c5d
adi_purnomo <adi.konsultan@work.bri.co.id>
2021-11-21 00:28:32 +0700

    Add automation install docker

---
+++ Automation Shell/install-docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y install \
+sudo apt-transport-https \
+ca-certificates \
+curl \
+software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) \
+stable"
+sudo apt update
+sudo apt install -y docker-ce
+sudo docker version
